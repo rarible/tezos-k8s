a6fb8af5ad7466ab575b64b7ee30696ffc5b53ef
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-09-22 08:38:04 -0700

    special activation with fitness 100

--- charts/tezos/scripts/chain-initiator.sh
+++ charts/tezos/scripts/chain-initiator.sh
@@ -8,13 +8,13 @@ set -x
 set -o pipefail
 if ! $CLIENT rpc get /chains/main/blocks/head/header | grep '"level": 0,'; then
     echo "Chain already activated, considering activation successful and exiting"
-    exit 0
+    #exit 0
 fi
 
 echo Activating chain:
 $CLIENT -d /var/tezos/client --block					\
 	genesis activate protocol					\
 	{{ .Values.activation.protocol_hash }}				\
-	with fitness -1 and key						\
+	with fitness 100 and key						\
 	$( cat /etc/tezos/activation_account_name )			\
 	and parameters /etc/tezos/parameters.json 2>&1 | head -200
